108ac90702c0d5336b475d8cc74a8046b347116c
pavelshikin <sh1k@bk.ru>
2021-07-19 13:58:07 +0300

    add next Config env - apiUrl

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    apiUrl: 'https://techno-train.herokuapp.com/'
+  }
+};
--- theme/index.ts
+++ theme/index.ts
@@ -1,29 +1,29 @@
-import {createMuiTheme, Theme, withStyles} from '@material-ui/core'
-import color from "./palette.module.scss";
-import IconButton from '@material-ui/core/IconButton'
+import { createTheme, Theme, withStyles } from '@material-ui/core';
+import color from './palette.module.scss';
+import IconButton from '@material-ui/core/IconButton';
 
-export const theme: Theme  = createMuiTheme({
-   palette: {
-      primary: {
-         main: color.primary,
-         contrastText: color.text
-      },
-      secondary: {
-         main: color.secondary
-      },
-      background: {
-         default: color.bgMain,
-         paper: color.bgBody,
-      }
-   }
-})
+export const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: color.primary,
+      contrastText: color.text
+    },
+    secondary: {
+      main: color.secondary
+    },
+    background: {
+      default: color.bgMain,
+      paper: color.bgBody
+    }
+  }
+});
 
 export const RedBtn = withStyles({
-   root: {
-      color: color.red,
-      padding: 0,
-      "&:hover": {
-         color: color.redHover
-      }
-   }
-})(IconButton)
+  root: {
+    color: color.red,
+    padding: 0,
+    '&:hover': {
+      color: color.redHover
+    }
+  }
+})(IconButton);
